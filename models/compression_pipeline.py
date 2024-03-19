4bc6e73133c3227f336c0a195c37ce14c7adca35
Dymytriy Zyunkin <dzyunkin@gmail.com>
2024-03-19 16:04:20 -0400

    cleaned pruning

--- models/compression_pipeline.py
+++ models/compression_pipeline.py
@@ -5,13 +5,12 @@ from torch.utils.data import DataLoader
 import copy
 
 
-class ModelPruner:
+class ModelCompressor:
     def __init__(self, model: Module, test_loader: DataLoader) -> None:
         self.model = model
         self.test_loader = test_loader
 
-    def prune_model(self, pruning_ratio: float) -> None:
-
+    def prune_model(self, pruning_ratio: float, pruning_type: str) -> None:
         model_copy = copy.deepcopy(self.model)
 
         parameters_to_prune = [
@@ -21,15 +20,38 @@ class ModelPruner:
             or isinstance(module, torch.nn.Linear)
         ]
 
-        prune.global_unstructured(
-            parameters_to_prune,
-            pruning_method=prune.L1Unstructured,
-            amount=pruning_ratio,
-        )
+        if pruning_type == "L1":
+            prune.global_unstructured(
+                parameters_to_prune,
+                pruning_method=prune.L1Unstructured,
+                amount=pruning_ratio,
+            )
+
+        elif pruning_type == "L2":
+            for module, parameter_name in parameters_to_prune:
+                prune.ln_structured(
+                    module,
+                    name=parameter_name,
+                    amount=pruning_ratio,
+                    n=2,
+                    dim=0,
+                )
+
+        elif pruning_type == "random":
+            prune.global_unstructured(
+                parameters_to_prune,
+                pruning_method=prune.RandomUnstructured,
+                amount=pruning_ratio,
+            )
+
+        else:
+            raise ValueError(f"Unsupported pruning type: {pruning_type}")
 
         # Remove the pruning reparametrization before saving the state_dict
         for module, _ in parameters_to_prune:
             prune.remove(module, "weight")
 
-        file_path = f"saved_models/resnet_model_pruned_{pruning_ratio}.pth"
+        file_path = (
+            f"saved_models/resnet_model_pruned_{pruning_type}_{pruning_ratio}.pth"
+        )
         torch.save(model_copy.state_dict(), file_path)
